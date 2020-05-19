8c2dabe75c2c80deaaf1b4cf7ac38a1048174f03
Christian Roberts <christian.m.roberts@outlook.com>
2020-05-19 14:03:30 -0400

    Gave birth to the JobSearchTrackerAPIContext.  Installed Microsoft.EntityFrameworkCore packages

---
+++ JobSearchTrackerAPI/JobSearchTrackerAPI/Data/JobSearchTrackerAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JobSearchTrackerAPI.Models;
+
+namespace JobSearchTrackerAPI.Data
+{
+    public class CADFundamentalsWebAPIContext : DbContext
+    {
+        public CADFundamentalsWebAPIContext(DbContextOptions<CADFundamentalsWebAPIContext> options)
+            : base(options)
+        {
+        }
+    }
+}
