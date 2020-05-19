9e938c4d551a1aa1e1e6170c784a402f20346d08
Christian Roberts <christian.m.roberts@outlook.com>
2020-05-19 13:54:57 -0400

    Gave birth to Groundwork

---
+++ JobSearchTrackerAPI/JobSearchTrackerAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JobSearchTrackerAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
